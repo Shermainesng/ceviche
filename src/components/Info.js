1e0f311ce9bba5419856935cb2488ce3606a6dff
Shermainesng <shersmallsng@gmail.com>
2022-04-24 23:34:54 +0800

    info-cards responsive

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
+import Media from "react-media";
 
 function Info() {
   return (
     <div className="container">
-      <div className="row py-5">
-        <div className="col-6 col-md-4 col-lg-3">
-          <div className="custom-info-img info-1-img"></div>
+      <div className="row py-5 justify-content-center">
+        <div className="col-8 col-sm-5 col-md-4 col-lg-3">
+          <div className="custom-info-img info-1-img mb-3"></div>
         </div>
-        <div className="col-8 col-md-8 col-lg-6">
+        <div className="col-8 col-md-4 col-lg-4 align-self-center">
           <div className="info-text">
             <h2>What is sustainable seafood?</h2>
             <p>Sustainable seafood ensures that fisheries have time to replenish and causes ensures minimal damage to ecosystems,while supporting the livelihoods of coastal communities.</p>
@@ -15,23 +16,53 @@ function Info() {
         </div>
       </div>
 
-      <div className="row py-5">
-        <div className="col-6 col-md-4 col-lg-3">
-          <div className="custom-info-img info-2-img"></div>
-        </div>
-        <div className="col-8 col-md-8 col-lg-6">
-          <div className="info-text">
-            <h2>Where does our seafood come from?</h2>
-            <p>Seafood is the most affordable and accessible protein on the planet. Yet, we don’t even know where it comes from.</p>
-          </div>
-        </div>
-      </div>
+      <Media
+        queries={{
+          small: "(max-width: 768px)",
+          large: "(min-width: 769px)"
+        }}
+      >
+        {matches => (
+          <Fragment>
+            {matches.small && (
+              <div>
+                <div className="row pb-5 justify-content-center hiding-block">
+                  <div className="col-8 col-sm-5 col-md-4 col-lg-3">
+                    <div className="custom-info-img info-2-img mb-3"></div>
+                  </div>
+                  <div className="col-8 col-md-4 col-lg-4 align-self-center">
+                    <div className="info-text">
+                      <h2>Where does our seafood come from?</h2>
+                      <p>Seafood is the most affordable and accessible protein on the planet. Yet, we don’t even know where it comes from.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+            {matches.large && (
+              <div>
+                <div className="row pb-5 justify-content-center hiding-block">
+                  <div className="col-8 col-md-4 col-lg-4 align-self-center">
+                    <div className="info-text">
+                      <h2>Where does our seafood come from?</h2>
+                      <p>Seafood is the most affordable and accessible protein on the planet. Yet, we don’t even know where it comes from.</p>
+                    </div>
+                  </div>
+                  <div className="col-8 col-sm-5 col-md-4 col-lg-3">
+                    <div className="custom-info-img info-2-img mb-3"></div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </Fragment>
+        )}
+      </Media>
 
-      <div className="row py-5">
-        <div className="col-6 col-md-4 col-lg-3">
-          <div className="custom-info-img info-3-img"></div>
+      <div className="row pb-5 justify-content-center">
+        <div className="col-8 col-sm-5 col-md-4 col-lg-3">
+          <div className="custom-info-img info-3-img mb-3"></div>
         </div>
-        <div className="col-8 col-md-8 col-lg-6">
+        <div className="col-8 col-md-4 col-lg-4 align-self-center">
           <div className="info-text">
             <h2>What fish to buy?</h2>
             <p>Knowing what kind of fish you are buying is the first step to making a sustainable choice.</p>
