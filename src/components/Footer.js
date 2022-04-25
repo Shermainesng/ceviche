0fd067af6ea8265adb33be519b51e5872f03e46c
Shermainesng <shersmallsng@gmail.com>
2022-04-25 23:04:24 +0800

    push to netlify

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
