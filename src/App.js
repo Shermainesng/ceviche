77c03168cdaa5c34ea52b031c2c345d7e3f35564
Shermainesng <shersmallsng@gmail.com>
2022-04-25 22:47:01 +0800

    footer created

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
 import "./Navbar.css";
+import "./Footer.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
@@ -13,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+
+function Footer() {
+  return (
+    <div className="container-fluid">
+      <div className="row footer-row-custom justify-content-around pt-3">
+        <div className="col-4 col-md-4 col-lg-3">
+          <div className="useful-links">
+            <p>
+              <strong>Useful Links</strong>
+              <br />
+              Sustainable Seafood Guide
+              <br />
+              About
+              <br />
+              FAQ
+              <br />
+            </p>
+          </div>
+        </div>
+        <div className="col-4 icons-list pt-3 pe-4">
+          <FontAwesomeIcon className="envelope pe-1" icon={faEnvelope} />
+          <FontAwesomeIcon className="instalogo" icon={faInstagram} />
+          <br />
+          <p className="small-text">&copy; ceviche.com</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
